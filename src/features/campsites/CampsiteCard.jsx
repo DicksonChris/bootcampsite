452092ff9b8d11cb239698ba51d26b1096e52487
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2022-05-07 11:07:00 -0500

    Workshop 1
    Adds Header, Campsite Cards, and Footer

---
+++ src/features/campsites/CampsiteCard.jsx
@@ -0,0 +1,14 @@
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap'
+
+const CampsiteCard = ({ campsite: { image, name } }) => {
+  return (
+    <Card>
+      <CardImg width='100%' src={image} alt={name} />
+      <CardImgOverlay>
+        <CardTitle>{name}</CardTitle>
+      </CardImgOverlay>
+    </Card>
+  )
+}
+
+export default CampsiteCard
---
+++ src/features/campsites/CampsitesList.jsx
@@ -0,0 +1,17 @@
+import { CAMPSITES } from '../../app/shared/CAMPSITES'
+import { Col, Row } from 'reactstrap'
+import CampsiteCard from './CampsiteCard'
+
+const CampsitesList = () => {
+  return (
+    <Row className='ms-auto'>
+      {CAMPSITES.map((campsite) => (
+        <Col className='m-4' md='5' key={campsite.id}>
+          <CampsiteCard campsite={campsite} />
+        </Col>
+      ))}
+    </Row>
+  )
+}
+
+export default CampsitesList
