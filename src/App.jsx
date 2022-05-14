35c368d0c78ce5af94764ed6cbdaa715b8eddcd6
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2022-05-14 10:42:24 -0500

    Week 2 workshop: about page, partners

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import ContactPage from './pages/ContactPage'
@@ -7,6 +6,7 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage'
 import CampsiteDetailPage from './pages/CampsiteDetailPage'
+import AboutPage from './pages/AboutPage'
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/contact' element={<ContactPage />} />
         <Route path='/directory' element={<CampsitesDirectoryPage />} />
+        <Route path='/about' element={<AboutPage />} />
         <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
       </Routes>
       <Footer />
