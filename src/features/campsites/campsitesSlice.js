12686eeb3cc98f201f0ebf31663e913cdec8c7b0
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2022-05-24 23:35:57 -0500

    Redux campsitesSlice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,13 +1,25 @@
-import { CAMPSITES } from '../../app/shared/CAMPSITES'
+import { createSlice } from "@reduxjs/toolkit"
+import { CAMPSITES } from "../../app/shared/CAMPSITES"
+
+const initialState = {
+    campsitesArray: CAMPSITES
+}
+
+const campsiteSlice = createSlice({
+    name: "campsites",
+    initialState
+})
+
+export const campsitesReducer = campsiteSlice.reducer
 
 export const selectAllCampsites = () => {
-  return CAMPSITES
+    return CAMPSITES
 }
 
 export const selectCampsiteById = (id) => {
-  return CAMPSITES.find((campsite) => campsite.id === parseInt(id))
+    return CAMPSITES.find((campsite) => campsite.id === parseInt(id))
 }
 
 export const selectFeaturedCampsite = () => {
-  return CAMPSITES.find(campsite => campsite.featured)
+    return CAMPSITES.find((campsite) => campsite.featured)
 }
