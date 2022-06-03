628b734409d6676de3c8d6b3cbece49a76954d89
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2022-06-02 21:11:34 -0500

    Fetch data json server

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import ContactPage from './pages/ContactPage'
-import HomePage from './pages/HomePage'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage'
-import CampsiteDetailPage from './pages/CampsiteDetailPage'
-import AboutPage from './pages/AboutPage'
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { Route, Routes } from "react-router-dom"
+import "./App.css"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import { fetchCampsites } from "./features/campsites/campsitesSlice"
+import AboutPage from "./pages/AboutPage"
+import CampsiteDetailPage from "./pages/CampsiteDetailPage"
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage"
+import ContactPage from "./pages/ContactPage"
+import HomePage from "./pages/HomePage"
 
 function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/contact' element={<ContactPage />} />
-        <Route path='/directory' element={<CampsitesDirectoryPage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
-      </Routes>
-      <Footer />
-    </div>
-  )
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchCampsites())
+    }, [dispatch])
+
+    return (
+        <div className='App'>
+            <Header />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/contact' element={<ContactPage />} />
+                <Route path='/directory' element={<CampsitesDirectoryPage />} />
+                <Route path='/about' element={<AboutPage />} />
+                <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+            </Routes>
+            <Footer />
+        </div>
+    )
 }
 
 export default App
